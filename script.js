7eb7069400c09b35112dcb4e5c4d3a391eea60ea
RiccardoMarroni <marroni.riccardo@gmail.com>
2024-03-21 14:12:49 +0100

    milestone 2

--- script.js
+++ script.js
@@ -39,3 +39,25 @@ const people = [
 ]
 
 console.log(people);
+
+let conUl = document.querySelector('#containerUl');
+console.log(conUl)
+
+for (let key of people) {
+    let liDati = document.createElement('li');
+
+    let nameElement = document.createElement('span');
+    nameElement.textContent = key.name;
+
+    let roleElement = document.createElement('span');
+    roleElement.textContent = key.role;
+
+    let imageElement = document.createElement('img');
+    imageElement.src = key.image;
+
+    liDati.appendChild(nameElement);
+    liDati.appendChild(roleElement);
+    liDati.appendChild(imageElement);
+
+    conUl.appendChild(liDati);
+}
